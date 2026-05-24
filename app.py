761a54841452ee61c6835a0e978b530cf4f32c48
SaanviS02 <st25195@ormiston.school.nz>
2026-05-24 18:58:31 +1200

    added code up till slide 41 --> added the flash messages, showed current cart and remove option + total price

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from flask import Flask, render_template, request, redirect, url_for, session, f
 app = Flask(__name__)
 app.secret_key = 'bananaleclerc'
 
+def calculate_total(cart):
+    total = sum(item['price'] * item['quantity'] for item in cart.values())
+    return total
+
 def load_data():
     with open('data/flowers.json') as file:
         flowers = json.load(file)
@@ -13,10 +17,13 @@ def load_data():
 
     return flowers, addons
 
+# load index.html
 @app.route('/')
 def index():
     flowers, addons = load_data()
-    return render_template('index.html', flowers=flowers, addons=addons)
+    cart = session.get('cart', {})
+    total = calculate_total(cart)
+    return render_template('index.html', flowers=flowers, addons=addons, cart=cart, total=total)
 
 # add selected flower to shopping cart
 @app.route('/add_to_cart', methods=['POST'])
@@ -31,16 +38,30 @@ def add_to_cart():
         return redirect(url_for('index'))
 
     if flower in cart:
-        cart[flower] ['quantity'] += quantity
+        cart[flower] ['quantity'] += quantity #add existing quantity
     else:
         cart[flower] = {
             'price': flowers[flower] ['price'],
             'quantity': quantity
         }
 
-    session['cart'] = cart
-    session.modified = True
+    session['cart'] = cart # update session
+    session.modified = True # force flask to save it
     flash(f"{quantity} {flower}(s) added to cart.")
+    return redirect(url_for('index'))
+
+# remove an item from the shopping cart
+@app.route('/remove_from_cart<item>')
+def remove_from_cart(item):
+    cart = session.get('cart', {})
+
+    if item in cart:
+        del cart[item]
+        session['cart'] = cart
+        session.modified = True
+        flash(f"Removed all {item.capitalize()} from the cart.")
+    else:
+        flash("Item not found in cart")
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
